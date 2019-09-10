ff5e50a9928041d5a66a2489c5c55e7c47d3ca2e
Damian E. Desario <ddesario@pespeciales-bcba.com>
2019-09-10 17:16:00 -0300

    practicando pedido chiqui

---
+++ projhtml/Clase 3/index.php
@@ -0,0 +1,30 @@
+<?php
+    INCLUDE './clases/lote.php';
+    INCLUDE './clases/loteDao.php';
+
+   // $archivo = fopen("./prueba.txt", "a");
+   // $rta=fwrite($archivo,"hol1a");
+   // fclose($archivo);
+
+
+    $request = ($_SERVER['REQUEST_METHOD']);
+
+    $dao = new LoteDao(".\lotemantis.csv");
+switch($request){
+    case "POST" : 
+        if(isset($_POST["Lote"]) && isset($_POST["Numero SLRG"]) && isset($_POST["Numero CUFE"]) && isset($_POST["Nombre SLRG"])&& isset($_POST["Modulo"]))
+        {
+            $lote = new Lote($_POST["Lote"], $_POST["Numero SLRG"], $_POST["Numero CUFE"],$_POST["Nombre SLRG"],$_POST["Modulo"]);
+            //$dao->guardar($lote); 
+            $lote->saludar(); 
+
+        }
+        break;
+    case "GET" :
+        $archivo = $dao->listar();
+        $array = json_decode($archivo);
+        var_dump($archivo);
+        break;
+       
+        
+}
