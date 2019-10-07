034ca1391e042c8136bb46ba7043aa3c62f378b1
Damian E. Desario <ddesario@pespeciales-bcba.com>
2019-10-07 16:44:52 -0300

    test

---
+++ Practica PrePa/public/js/anuncio.js
@@ -0,0 +1,10 @@
+function Anuncio(id, titulo, descripcion, precio, num_wc, num_dormitorio, num_estacionamiento, transaccion) {
+    this.id = id;
+    this.titulo = titulo;
+    this.transaccion = transaccion
+    this.descripcion = descripcion;
+    this.precio = precio;
+    this.num_wc = num_wc;
+    this.num_dormitorio = num_dormitorio;
+    this.num_estacionamiento = num_estacionamiento;
+}
